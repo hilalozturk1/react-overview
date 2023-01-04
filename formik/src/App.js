d3e699687cd7608a7c3f02983c7e9155393710ce
hilalozturk1 <hlloztrk68@gmail.com>
2023-01-04 16:06:12 +0300

    formik form validation with yup

--- formik/src/App.js
+++ formik/src/App.js
@@ -1,29 +1,31 @@
 import { Formik, Field, Form, useFormik } from "formik";
 import "./App.css";
+import * as yup from "yup";
 
 function App() {
+  const schema = yup.object().shape({
+    email: yup.string().email().required(),
+    password: yup.string().min(5).required(),
+    passwordConfirm: yup
+      .string()
+      .oneOf([yup.ref("password")])
+      .required(),
+  });
   const formik = useFormik({
     initialValues: {
-      firstName: "",
-      lastName: "",
       email: "",
-      gender: "male",
-      hobies: [],
-      country: "tr",
+      password: "",
+      passwordConfirm: "",
     },
     onSubmit: (values) => {
       console.log(JSON.stringify(values, null, 2));
     },
+    validationSchema: schema,
   });
   return (
     <div className="App">
       <h1>Sign Up</h1>
       <form onSubmit={formik.handleSubmit}>
-        <label htmlFor="firstName">First Name</label>
-        <input name="firstName" placeholder="Jane" onChange={formik.handleChange} />
-        <label htmlFor="lastName">Last Name</label>
-        <input name="lastName" placeholder="Doe" onChange={formik.handleChange} />
-        <hr />
         <label htmlFor="email">Email</label>
         <input
           name="email"
@@ -32,33 +34,14 @@ function App() {
           onChange={formik.handleChange}
         />
         <br />
-        <hr />
-        Male
-        <input
-          name="gender"
-          value="male"
-          onChange={formik.handleChange}
-          type="radio"
-          checked={formik.values.gender === "male"}
-        />
-        Female
-        <input
-          name="gender"
-          value="female"
-          onChange={formik.handleChange}
-          type="radio"
-          checked={formik.values.gender === "female"}
-        />
         <br />
-        football
-        <input type="checkbox" name="hobies" value="football" onChange={formik.handleChange} />
-        voleyball
-        <input type="checkbox" name="hobies" value="voleyball" onChange={formik.handleChange} />
+        <label>Password</label>
+        <input name="password" onChange={formik.handleChange} />
         <br />
-        <select name="country" onChange={formik.handleChange}>
-          <option value="tr">TR</option>
-          <option value="uk">UK</option>
-        </select>
+        <br />
+        <label>Confirm Password</label>
+        <input name="passwordConfirm" onChange={formik.handleChange} />
+        <br />
         <br />
         <button type="submit">Submit</button>
         <hr />
