2ebc9865ccf0bcf1a0ae9343474c1dc3689cf82e
hilalozturk1 <hlloztrk68@gmail.com>
2023-01-04 16:24:46 +0300

    formik error messages

--- formik/src/App.js
+++ formik/src/App.js
@@ -4,7 +4,7 @@ import * as yup from "yup";
 
 function App() {
   const schema = yup.object().shape({
-    email: yup.string().email().required(),
+    email: yup.string().email("email must be a valid email!!!").required("Required field"),
     password: yup.string().min(5).required(),
     passwordConfirm: yup
       .string()
@@ -32,15 +32,21 @@ function App() {
           placeholder="jane@acme.com"
           type="email"
           onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
         />
+        {formik.errors.email && formik.touched.email && <div>{formik.errors.email}</div>}
         <br />
         <br />
         <label>Password</label>
-        <input name="password" onChange={formik.handleChange} />
+        <input name="password" onChange={formik.handleChange} onBlur={formik.handleBlur} />
+        {formik.errors.password && formik.touched.password && <div>{formik.errors.password}</div>}
         <br />
         <br />
         <label>Confirm Password</label>
-        <input name="passwordConfirm" onChange={formik.handleChange} />
+        <input name="passwordConfirm" onChange={formik.handleChange} onBlur={formik.handleBlur} />
+        {formik.errors.passwordConfirm && formik.touched.passwordConfirm && (
+          <div>{formik.errors.passwordConfirm}</div>
+        )}
         <br />
         <br />
         <button type="submit">Submit</button>
