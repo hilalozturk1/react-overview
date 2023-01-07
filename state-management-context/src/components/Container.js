e86d9b788e7d3e5b573fe95f2a8c9fbb7c0fa702
hilalozturk1 <hlloztrk68@gmail.com>
2023-01-07 16:20:26 +0300

    State management - multi context -log in & log out

--- state-management-context/src/components/Container.js
+++ state-management-context/src/components/Container.js
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import ThemeContext from "../contex/ThemeContext";
 import Button from "./Button";
 import Header from "./Header";
+import Profile from "./Profile";
 
 function Container() {
   const { theme } = useContext(ThemeContext);
@@ -10,6 +11,7 @@ function Container() {
       {theme}
       <Header />
       <Button />
+      <Profile />
     </div>
   );
 }
---
+++ state-management-context/src/components/Profile.js
@@ -0,0 +1,35 @@
+import { useContext, useState } from "react";
+import UserContext from "../contex/UserContext";
+
+function Profile() {
+  const { user, setUser } = useContext(UserContext);
+  const [loading, setLoading] = useState(false);
+  console.log(user);
+
+  const handleLogin = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setUser({ is: 1, userName: "dsf", bio: "sdfs" });
+      setLoading(false);
+    }, 1500);
+  };
+  const logout = () => {
+    setUser(null)
+  };
+
+  return (
+    <div>
+      Profile <hr />
+      {!user && (
+        <>
+          <button onClick={handleLogin}>Login</button> <hr /> {loading && <div>Loading..</div>}
+        </>
+      )}
+      <code>{JSON.stringify(user)}</code>
+      <hr />
+      {user && <button onClick={logout}>Logout</button>}
+    </div>
+  );
+}
+
+export default Profile;
