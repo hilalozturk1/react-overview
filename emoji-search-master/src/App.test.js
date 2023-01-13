345bfd3ff56fc543b316f4f6efae10d91a5c3d57
hilalozturk1 <hlloztrk68@gmail.com>
2023-01-13 15:59:15 +0300

    filtering test case

--- emoji-search-master/src/App.test.js
+++ emoji-search-master/src/App.test.js
@@ -1,5 +1,5 @@
 /* eslint-disable testing-library/no-render-in-setup */
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import App from "./App";
 
@@ -7,4 +7,14 @@ it("emoji results should be rendered correctly", () => {
   render(<App />);
   const items = screen.getAllByText("Click to copy emoji");
   expect(items.length).toEqual(20);
+});
+
+it("Filtering should be re-rendered", () => {
+  render(<App />);
+  const emoji = "smile";
+  const input = screen.getByRole("textbox");
+  fireEvent.change(input, { target: { value: emoji } });
+
+  const items = screen.getAllByText("Click to copy emoji");
+  expect(items.length).toBeLessThan(1820);
 });
